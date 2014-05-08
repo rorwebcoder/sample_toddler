4ef0decb22c2cb610c68636d70a58f0538fe9e7c
udhayakumar <udhayakumar.dhanabalan@gmail.com>
2014-05-07 22:41:24 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleToddler::Application.config.secret_token = '6c74e2306987bbda2da7b0d226fc45e163e23d2cdd8833343b3ea18ee307c6b8a1ca0efb313f6b28e7f24229552573f0e1ad142a34d90a71b054d559734ae813'
